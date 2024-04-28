9486df23698a09de795cc6b3027cf45677a412c2
Xolikov Abdulbosit <abdulbositkhalikov96@gmail.com>
2024-04-28 17:33:03 +0500

    Update

--- BankAPI/settings.py
+++ BankAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$fqxs-n4^ct1i#!g#ivkl$x^t7v-%zi=-p^xora%tg-==qs3ba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
